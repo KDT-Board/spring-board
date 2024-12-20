73d2a2e1efe6d7d0af83cc1e1697fb8873134994
KSJ0128 <ksj01128@naver.com>
2024-12-20 15:38:58 +0900

    chore: 기본 프로젝트 세팅

---
+++ Boad/src/main/java/kdt/boad/BoadApplication.java
@@ -0,0 +1,13 @@
+package kdt.boad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoadApplication.class, args);
+    }
+
+}
