0dc8750ea17b69691fc03e9897dd0c2e38b71f4b
KSJ0128 <ksj01128@naver.com>
2024-12-25 01:42:45 +0900

    test: UserRepository 테스트 코드 구현
    - existsById, existsByNickname, findById 관련 테스트 코드 작성

---
+++ Boad/src/test/java/kdt/boad/user/repository/UserRepositoryTest.java
@@ -0,0 +1,124 @@
+package kdt.boad.user.repository;
+
+import kdt.boad.user.domain.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest // 트랜잭션 마치면 롤백 수행, 레포지토리 테스트 수행 용도
+class UserRepositoryTest {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    @DisplayName("저장된 사용자 ID 존재 여부 확인 - 존재하는 사용자 : True")
+    void existsById_return_true() {
+        // Given
+        User saveUser = User.builder()
+                .id("testUser1")
+                .password("TestUser1!")
+                .nickname("testUser1")
+                .build();
+        userRepository.save(saveUser);
+
+        // When
+        boolean exist = userRepository.existsById("testUser1");
+
+        // Then
+        assertTrue(exist);
+    }
+
+    @Test
+    @DisplayName("저장된 사용자 ID 존재 여부 확인 - 존재하는 사용자 : False")
+    void existsById_return_false() {
+        // Given
+        User saveUser = User.builder()
+                .id("testUser1")
+                .password("TestUser1!")
+                .nickname("testUser1")
+                .build();
+        userRepository.save(saveUser);
+
+        // When
+        boolean exist = userRepository.existsById("testUser2");
+
+        // Then
+        assertFalse(exist);
+    }
+
+    @Test
+    @DisplayName("저장된 사용자 닉네임 존재 여부 확인 - 존재하는 사용자 : True")
+    void existsByNickname_return_true() {
+        // Given
+        User saveUser = User.builder()
+                .id("testUser1")
+                .password("TestUser1!")
+                .nickname("testUser1")
+                .build();
+        userRepository.save(saveUser);
+
+        // When
+        boolean exist = userRepository.existsByNickname("testUser1");
+
+        // Then
+        assertTrue(exist);
+    }
+
+    @Test
+    @DisplayName("저장된 사용자 닉네임 존재 여부 확인 - 존재하지 않는 사용자 : False")
+    void existsByNickname_return_false() {
+        // Given
+        User saveUser = User.builder()
+                .id("testUser1")
+                .password("TestUser1!")
+                .nickname("testUser1")
+                .build();
+        userRepository.save(saveUser);
+
+        // When
+        boolean exist = userRepository.existsByNickname("testUser2");
+
+        // Then
+        assertFalse(exist);
+    }
+
+    @Test
+    @DisplayName("id를 통한 사용자 조회 - 존재하지 않는 사용자 : True")
+    void findById_non_exist_user() {
+        // Given
+        User saveUser = User.builder()
+                .id("testUser1")
+                .password("TestUser1!")
+                .nickname("testUser1")
+                .build();
+        userRepository.save(saveUser);
+
+        // When
+        User findUser = userRepository.findById("NoneUser");
+
+        // Then
+        assertThat(findUser).isEqualTo(null);
+    }
+
+    @Test
+    @DisplayName("id를 통한 사용자 조회 - 존재하는 사용자 : True")
+    void findById_exist_user() {
+        // Given
+        User saveUser = User.builder()
+                .id("testUser1")
+                .password("TestUser1!")
+                .nickname("testUser1")
+                .build();
+        userRepository.save(saveUser);
+
+        // When
+        User findUser = userRepository.findById("testUser1");
+
+        // Then
+        assertThat(findUser).isEqualTo(saveUser);
+    }
+}
