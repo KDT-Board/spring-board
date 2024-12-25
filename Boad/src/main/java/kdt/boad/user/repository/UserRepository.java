e0837e5ffc44e86b3d20e94852ca670d527cdea2
KSJ0128 <ksj01128@naver.com>
2024-12-25 23:18:34 +0900

    feat: Controller 구현
    UserViewController를 통해 리다이렉트 등의 로직을 구현했습니다.

--- Boad/src/main/java/kdt/boad/user/repository/UserRepository.java
+++ Boad/src/main/java/kdt/boad/user/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package kdt.boad.user.repository;
 
+import kdt.boad.user.domain.Grade;
 import kdt.boad.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -7,4 +8,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsById(String id);
     boolean existsByNickname(String nickname);
     User findById(String id);
+    long count();
+    Long countByGrade(Grade grade);
 }
